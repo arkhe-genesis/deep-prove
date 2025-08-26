2d4b0c25dc95ca121c3d84c8dcfdd9e88ce4344d
delehef <franklin.delehelle@odena.eu>
2025-08-26 18:44:19 +0300

    build: `protoc` is still a required dependency for ONNX parsing (#79)

--- devenv.nix
+++ devenv.nix
@@ -7,7 +7,7 @@
     # General dev.
     pkgs.git pkgs.git-lfs pkgs.openssl pkgs.cmake
     # Rust crates build deps
-    pkgs.openssl pkgs.llvmPackages.libclang.lib
+    pkgs.openssl pkgs.llvmPackages.libclang.lib pkgs.protobuf
   ];
 
   env = {
